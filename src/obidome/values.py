b47d3e55016eadddf027f3e7fe1ccc229e54e564
Hisano Kujirai <mail@rutto-ysgr.net>
2026-01-15 15:23:32 +0900

    Add cpu demanding process name

--- src/obidome/values.py
+++ src/obidome/values.py
@@ -1,5 +1,6 @@
 """System values module."""
 
+import ctypes
 from logging import getLogger
 
 import psutil
@@ -13,6 +14,12 @@ class LazySystemValueFetcher(dict):
         super().__init__()
         self.logger = getLogger(__name__)
         self._cache = {}
+
+        self.is_admin = ctypes.windll.shell32.IsUserAnAdmin()
+        if not self.is_admin:
+            self.logger.warning(
+                "Application is not running with administrator privileges. Some values may be unavailable."
+            )
 
     def __getitem__(self, key: str) -> float | int | str:
         """Fetch the system value for the given key."""
@@ -73,3 +80,32 @@ class LazySystemValueFetcher(dict):
     def ram_used_gb(self) -> float:
         """Get the used RAM in gigabytes."""
         return self.ram_used / (1024 * 1024 * 1024)
+
+    @property
+    def cpu_demanding_process(self) -> str:
+        """Get the name of the most CPU-demanding process."""
+        if not self.is_admin:
+            return "N/A"
+
+        if "psutil.process_iter.cpu_percent" not in self._cache:
+            self._cache["psutil.process_iter.cpu_percent"] = sorted(
+                psutil.process_iter(attrs=["pid", "name", "cpu_percent"]),
+                key=lambda p: p.info["cpu_percent"],
+                reverse=True,
+            )
+        return self._cache["psutil.process_iter.cpu_percent"][0].info["name"]
+
+
+    @property
+    def cpu_demanding_process_cpu_percent(self) -> float:
+        """Get the CPU usage percentage of the most CPU-demanding process."""
+        if not self.is_admin:
+            return float("nan")
+
+        if "psutil.process_iter.cpu_percent" not in self._cache:
+            self._cache["psutil.process_iter.cpu_percent"] = sorted(
+                psutil.process_iter(attrs=["pid", "name", "cpu_percent"]),
+                key=lambda p: p.info["cpu_percent"],
+                reverse=True,
+            )
+        return self._cache["psutil.process_iter.cpu_percent"][0].info["cpu_percent"]
