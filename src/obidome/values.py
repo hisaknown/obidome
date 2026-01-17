70838150fbae4828181b5019ffcde59094195989
Hisano Kujirai <mail@rutto-ysgr.net>
2026-01-17 11:35:27 +0900

    Improve caching

--- src/obidome/values.py
+++ src/obidome/values.py
@@ -2,6 +2,7 @@
 
 import ctypes
 import subprocess
+from collections.abc import Callable
 from logging import getLogger
 from time import time
 
@@ -55,9 +56,8 @@ class LazySystemValueFetcher(dict):
         self.logger.info("System value fetcher settings loaded.")
 
     def __getitem__(self, key: str) -> float | int | str:
-        """Fetch the system value for the given key."""
+        """Fetch an attribute value by its key. If not found, try to execute a custom command."""
         value = getattr(self, key, None)
-
         if value is not None:
             return value
 
@@ -69,20 +69,38 @@ class LazySystemValueFetcher(dict):
         self.logger.warning("Requested unknown system value: %s", key)
         return "N/A"
 
+    @staticmethod
+    def property_with_cache(func: Callable) -> property:
+        """Decorate a property to use caching for the given key."""
+        def wrapper(self: "LazySystemValueFetcher") -> object:
+            key = func.__name__
+            if (v := self.load_from_cache(key)) is not None:
+                return v
+            value = func(self)
+            self.put_to_cache(key, value)
+            return value
+        return property(wrapper)
+
+    def load_from_cache(self, key: str) -> int | float| str | None:
+        """Load a value from the cache if it exists."""
+        return self._cache.get(key, None)
+
+    def put_to_cache(self, key: str, value: float | str) -> None:
+        """Put a value into the cache."""
+        self._cache[key] = value
+
     def clear_cache(self) -> None:
         """Clear the cached system values."""
         self._cache.clear()
 
     # --- CPU ---
 
-    @property
+    @property_with_cache
     def cpu_percent(self) -> float:
         """Get the current CPU usage percentage."""
-        if "psutil.cpu_percent" not in self._cache:
-            self._cache["psutil.cpu_percent"] = psutil.cpu_percent(interval=None)
-        return self._cache["psutil.cpu_percent"]
+        return psutil.cpu_percent(interval=None)
 
-    @property
+    @property_with_cache
     def cpu_percent_plot(self) -> str:
         """Get a sparkline plot of the current CPU usage percentage."""
         if not hasattr(self, "_cpu_percent_plotter"):
@@ -91,309 +109,285 @@ class LazySystemValueFetcher(dict):
 
     # --- RAM ---
 
-    @property
+    @property_with_cache
     def ram_percent(self) -> float:
         """Get the current RAM usage percentage."""
-        if "psutil.virtual_memory" not in self._cache:
-            self._cache["psutil.virtual_memory"] = psutil.virtual_memory()
-        return self._cache["psutil.virtual_memory"].percent
+        return psutil.virtual_memory().percent
 
-    @property
+    @property_with_cache
     def ram_percent_plot(self) -> str:
         """Get a sparkline plot of the current RAM usage percentage."""
         if not hasattr(self, "_ram_percent_plotter"):
             self._ram_percent_plotter = SparklineGenerator(self._ram_percent_plot_settings, min_val=0, max_val=100)
         return self._ram_percent_plotter.update_and_get_b64(self.ram_percent)
 
-    @property
+    @property_with_cache
     def ram_total(self) -> int:
         """Get the total RAM in bytes."""
-        if "psutil.virtual_memory" not in self._cache:
-            self._cache["psutil.virtual_memory"] = psutil.virtual_memory()
         return self._cache["psutil.virtual_memory"].total
 
-    @property
+    @property_with_cache
     def ram_total_mb(self) -> float:
         """Get the total RAM in megabytes."""
         return self.ram_total / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def ram_total_gb(self) -> float:
         """Get the total RAM in gigabytes."""
         return self.ram_total / (1024 * 1024 * 1024)
 
-    @property
+    @property_with_cache
     def ram_used(self) -> int:
         """Get the used RAM in bytes."""
-        if "psutil.virtual_memory" not in self._cache:
-            self._cache["psutil.virtual_memory"] = psutil.virtual_memory()
         return self._cache["psutil.virtual_memory"].used
 
-    @property
+    @property_with_cache
     def ram_used_mb(self) -> float:
         """Get the used RAM in megabytes."""
         return self.ram_used / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def ram_used_gb(self) -> float:
         """Get the used RAM in gigabytes."""
         return self.ram_used / (1024 * 1024 * 1024)
 
     # --- Process ---
 
-    @property
+    @property_with_cache
     def cpu_demanding_process(self) -> str:
         """Get the name of the most CPU-demanding process."""
         if not self.is_admin:
             return "N/A"
 
-        if "psutil.process_iter.cpu_percent" not in self._cache:
-            self._cache["psutil.process_iter.cpu_percent"] = sorted(
+        processes = sorted(
                 psutil.process_iter(attrs=["pid", "name", "cpu_percent"]),
                 key=lambda p: p.info["cpu_percent"],
                 reverse=True,
             )
-        return self._cache["psutil.process_iter.cpu_percent"][0].info["name"]
+        return processes[0].info["name"]
 
-    @property
+    @property_with_cache
     def cpu_demanding_process_cpu_percent(self) -> float:
         """Get the CPU usage percentage of the most CPU-demanding process."""
         if not self.is_admin:
             return float("nan")
 
-        if "psutil.process_iter.cpu_percent" not in self._cache:
-            self._cache["psutil.process_iter.cpu_percent"] = sorted(
+        processes = sorted(
                 psutil.process_iter(attrs=["pid", "name", "cpu_percent"]),
                 key=lambda p: p.info["cpu_percent"],
                 reverse=True,
             )
-        return self._cache["psutil.process_iter.cpu_percent"][0].info["cpu_percent"]
+        return processes[0].info["cpu_percent"]
 
     # --- Network ---
 
-    @property
+    @property_with_cache
     def network_bytes_sent(self) -> int:
         """Get the total number of bytes sent over the network."""
-        if "psutil.net_io_counters" not in self._cache:
-            self._cache["psutil.net_io_counters"] = psutil.net_io_counters()
-        return self._cache["psutil.net_io_counters"].bytes_sent
+        return psutil.net_io_counters().bytes_sent
 
-    @property
+    @property_with_cache
     def network_kb_sent(self) -> float:
         """Get the total number of kilobytes sent over the network."""
         return self.network_bytes_sent / 1024
 
-    @property
+    @property_with_cache
     def network_mb_sent(self) -> float:
         """Get the total number of megabytes sent over the network."""
         return self.network_bytes_sent / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def network_bytes_sent_per_sec(self) -> float:
         """Get the number of bytes sent over the network per second."""
-        if "psutil.net_io_counters" not in self._cache:
-            self._cache["psutil.net_io_counters"] = psutil.net_io_counters()
-        if not hasattr(self, "_network_bytes_sent_last"):
-            self._network_bytes_sent_last = [time(), self._cache["psutil.net_io_counters"].bytes_sent]
-            return 0.0
         current_time = time()
-        bytes_per_sec = (self._cache["psutil.net_io_counters"].bytes_sent - self._network_bytes_sent_last[1]) / (
+        if not hasattr(self, "_network_bytes_sent_last"):
+            self._network_bytes_sent_last = [current_time, self.network_bytes_sent]
+            return 0.0
+        bytes_per_sec = (psutil.net_io_counters().bytes_sent - self._network_bytes_sent_last[1]) / (
             current_time - self._network_bytes_sent_last[0]
         )
-        self._network_bytes_sent_last = [current_time, self._cache["psutil.net_io_counters"].bytes_sent]
+        self._network_bytes_sent_last = [current_time, self.network_bytes_sent]
         return bytes_per_sec
 
-    @property
+    @property_with_cache
     def network_kb_sent_per_sec(self) -> float:
         """Get the number of kilobytes sent over the network per second."""
         return self.network_bytes_sent_per_sec / 1024
 
-    @property
+    @property_with_cache
     def network_mb_sent_per_sec(self) -> float:
         """Get the number of megabytes sent over the network per second."""
         return self.network_bytes_sent_per_sec / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def network_bytes_recv(self) -> int:
         """Get the total number of bytes received over the network."""
-        if "psutil.net_io_counters" not in self._cache:
-            self._cache["psutil.net_io_counters"] = psutil.net_io_counters()
-        return self._cache["psutil.net_io_counters"].bytes_recv
+        return psutil.net_io_counters().bytes_recv
 
-    @property
+    @property_with_cache
     def network_kb_recv(self) -> float:
         """Get the total number of kilobytes received over the network."""
         return self.network_bytes_recv / 1024
 
-    @property
+    @property_with_cache
     def network_mb_recv(self) -> float:
         """Get the total number of megabytes received over the network."""
         return self.network_bytes_recv / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def network_gb_recv(self) -> float:
         """Get the total number of gigabytes received over the network."""
         return self.network_bytes_recv / (1024 * 1024 * 1024)
 
-    @property
+    @property_with_cache
     def network_bytes_recv_per_sec(self) -> float:
         """Get the number of bytes received over the network per second."""
-        if "psutil.net_io_counters" not in self._cache:
-            self._cache["psutil.net_io_counters"] = psutil.net_io_counters()
-        if not hasattr(self, "_network_bytes_recv_last"):
-            self._network_bytes_recv_last = [time(), self._cache["psutil.net_io_counters"].bytes_recv]
-            return 0.0
         current_time = time()
-        bytes_per_sec = (self._cache["psutil.net_io_counters"].bytes_recv - self._network_bytes_recv_last[1]) / (
+        if not hasattr(self, "_network_bytes_recv_last"):
+            self._network_bytes_recv_last = [current_time, self.network_bytes_recv]
+            return 0.0
+        bytes_per_sec = (psutil.net_io_counters().bytes_recv - self._network_bytes_recv_last[1]) / (
             current_time - self._network_bytes_recv_last[0]
         )
-        self._network_bytes_recv_last = [current_time, self._cache["psutil.net_io_counters"].bytes_recv]
+        self._network_bytes_recv_last = [current_time, self.network_bytes_recv]
         return bytes_per_sec
 
-    @property
+    @property_with_cache
     def network_kb_recv_per_sec(self) -> float:
         """Get the number of kilobytes received over the network per second."""
         return self.network_bytes_recv_per_sec / 1024
 
-    @property
+    @property_with_cache
     def network_mb_recv_per_sec(self) -> float:
         """Get the number of megabytes received over the network per second."""
         return self.network_bytes_recv_per_sec / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def network_gb_recv_per_sec(self) -> float:
         """Get the number of gigabytes received over the network per second."""
         return self.network_bytes_recv_per_sec / (1024 * 1024 * 1024)
 
     # --- Disk I/O ---
 
-    @property
+    @property_with_cache
     def disk_io_read_bytes(self) -> int:
         """Get the total number of bytes read from disk."""
-        if "psutil.disk_io_counters" not in self._cache:
-            self._cache["psutil.disk_io_counters"] = psutil.disk_io_counters()
-        if self._cache["psutil.disk_io_counters"] is None:
+        counter = psutil.disk_io_counters()
+        if counter is None:
             return -1
-        return self._cache["psutil.disk_io_counters"].read_bytes
+        return counter.read_bytes
 
-    @property
+    @property_with_cache
     def disk_io_read_kb(self) -> float:
         """Get the total number of kilobytes read from disk."""
         if self.disk_io_read_bytes == -1:
             return -1
         return self.disk_io_read_bytes / 1024
 
-    @property
+    @property_with_cache
     def disk_io_read_mb(self) -> float:
         """Get the total number of megabytes read from disk."""
         if self.disk_io_read_bytes == -1:
             return -1
         return self.disk_io_read_bytes / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def disk_io_read_gb(self) -> float:
         """Get the total number of gigabytes read from disk."""
         if self.disk_io_read_bytes == -1:
             return -1
         return self.disk_io_read_bytes / (1024 * 1024 * 1024)
 
-    @property
+    @property_with_cache
     def disk_io_read_bytes_per_sec(self) -> float:
         """Get the number of bytes read from disk per second."""
-        if "psutil.disk_io_read_bytes_per_sec" in self._cache:
-            return self._cache["psutil.disk_io_read_bytes_per_sec"]
         if self.disk_io_read_bytes == -1:
             return -1.0
-        if not hasattr(self, "_disk_io_read_bytes_last"):
-            self._disk_io_read_bytes_last = [time(), self._cache["psutil.disk_io_counters"].read_bytes]
-            return 0.0
         current_time = time()
+        if not hasattr(self, "_disk_io_read_bytes_last"):
+            self._disk_io_read_bytes_last = [current_time, self.disk_io_read_bytes]
+            return 0.0
         bytes_per_sec = (self.disk_io_read_bytes - self._disk_io_read_bytes_last[1]) / (
             current_time - self._disk_io_read_bytes_last[0]
         )
-        self._disk_io_read_bytes_last = [current_time, self._cache["psutil.disk_io_counters"].read_bytes]
-        self._cache["psutil.disk_io_read_bytes_per_sec"] = bytes_per_sec
+        self._disk_io_read_bytes_last = [current_time, self.disk_io_read_bytes]
         return bytes_per_sec
 
-    @property
+    @property_with_cache
     def disk_io_read_kb_per_sec(self) -> float:
         """Get the number of kilobytes read from disk per second."""
         v = self.disk_io_read_bytes_per_sec
         return self.disk_io_read_bytes_per_sec / 1024 if v != -1.0 else -1.0
 
-    @property
+    @property_with_cache
     def disk_io_read_mb_per_sec(self) -> float:
         """Get the number of megabytes read from disk per second."""
         v = self.disk_io_read_bytes_per_sec
         return self.disk_io_read_bytes_per_sec / (1024 * 1024) if v != -1.0 else -1.0
 
-    @property
+    @property_with_cache
     def disk_io_read_gb_per_sec(self) -> float:
         """Get the number of gigabytes read from disk per second."""
         v = self.disk_io_read_bytes_per_sec
         return self.disk_io_read_bytes_per_sec / (1024 * 1024 * 1024) if v != -1.0 else -1.0
 
-    @property
+    @property_with_cache
     def disk_io_write_bytes(self) -> int:
         """Get the total number of bytes written to disk."""
-        if "psutil.disk_io_counters" not in self._cache:
-            self._cache["psutil.disk_io_counters"] = psutil.disk_io_counters()
-        if self._cache["psutil.disk_io_counters"] is None:
+        counter = psutil.disk_io_counters()
+        if counter is None:
             return -1
-        return self._cache["psutil.disk_io_counters"].write_bytes
+        return counter.write_bytes
 
-    @property
+    @property_with_cache
     def disk_io_write_kb(self) -> float:
         """Get the total number of kilobytes written to disk."""
         if self.disk_io_write_bytes == -1:
             return -1
         return self.disk_io_write_bytes / 1024
 
-    @property
+    @property_with_cache
     def disk_io_write_mb(self) -> float:
         """Get the total number of megabytes written to disk."""
         if self.disk_io_write_bytes == -1:
             return -1
         return self.disk_io_write_bytes / (1024 * 1024)
 
-    @property
+    @property_with_cache
     def disk_io_write_gb(self) -> float:
         """Get the total number of gigabytes written to disk."""
         if self.disk_io_write_bytes == -1:
             return -1
         return self.disk_io_write_bytes / (1024 * 1024 * 1024)
 
-    @property
+    @property_with_cache
     def disk_io_write_bytes_per_sec(self) -> float:
         """Get the number of bytes written to disk per second."""
-        if "psutil.disk_io_write_bytes_per_sec" in self._cache:
-            return self._cache["psutil.disk_io_write_bytes_per_sec"]
+        current_time = time()
         if self.disk_io_write_bytes == -1:
             return -1.0
         if not hasattr(self, "_disk_io_write_bytes_last"):
-            self._disk_io_write_bytes_last = [time(), self._cache["psutil.disk_io_counters"].write_bytes]
+            self._disk_io_write_bytes_last = [current_time, self.disk_io_write_bytes]
             return 0.0
-        current_time = time()
         bytes_per_sec = (self.disk_io_write_bytes - self._disk_io_write_bytes_last[1]) / (
             current_time - self._disk_io_write_bytes_last[0]
         )
-        self._disk_io_write_bytes_last = [current_time, self._cache["psutil.disk_io_counters"].write_bytes]
-        self._cache["psutil.disk_io_write_bytes_per_sec"] = bytes_per_sec
+        self._disk_io_write_bytes_last = [current_time, self.disk_io_write_bytes]
         return bytes_per_sec
 
-    @property
+    @property_with_cache
     def disk_io_write_kb_per_sec(self) -> float:
         """Get the number of kilobytes written to disk per second."""
         v = self.disk_io_write_bytes_per_sec
         return self.disk_io_write_bytes_per_sec / 1024 if v != -1.0 else -1.0
 
-    @property
+    @property_with_cache
     def disk_io_write_mb_per_sec(self) -> float:
         """Get the number of megabytes written to disk per second."""
         v = self.disk_io_write_bytes_per_sec
         return self.disk_io_write_bytes_per_sec / (1024 * 1024) if v != -1.0 else -1.0
 
-    @property
+    @property_with_cache
     def disk_io_write_gb_per_sec(self) -> float:
         """Get the number of gigabytes written to disk per second."""
         v = self.disk_io_write_bytes_per_sec
